d114f8052fb452cd9a01824ea5ef952b31d23f19
Yiming Zhao <zhaoyiming916@gmail.com>
2020-05-08 23:52:39 -0700

    added user interface

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from '../interfaces/user.interface';
 
 @Controller('users')
 export class UsersController {
     @Get()
-    getUsers() : object[] {
+    getUsers() : User[] {
         return [
             {'name': 'Yiming'},
             {'name': 'Greg'}
