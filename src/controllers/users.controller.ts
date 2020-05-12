aab77c6e664d1bfe1a22d19c494e2292fb0ba29f
Yiming Zhao <zhaoyiming916@gmail.com>
2020-05-11 23:29:28 -0700

    added users service

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { UsersResponseDTO } from './users.response.dto';
+import { User } from '../interfaces/user.interface';
+import { UsersService } from '../services/users.service';
 
 @Controller('users')
 export class UsersController {
-    
+  constructor(private usersService : UsersService) {}
+
   @Get()
-  getUsers(): UsersResponseDTO {
-    return new UsersResponseDTO([{ name: 'Yiming' }, { name: 'Greg' }]);
+  async getUsers(): Promise<User[]> {
+    return this.usersService.getAllUsers();
   }
 }
---
+++ src/services/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../interfaces/user.interface';
+
+@Injectable()
+export class UsersService {
+    public readonly users: User[] = [];
+
+    addUser(user: User) {
+        this.users.push(user);
+    }
+    
+    getAllUsers(): User[] {
+        return this.users;
+    }
+}
