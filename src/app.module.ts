ad9af19cebf688c9b5bc5adb2daa7c0e08a62395
Yiming Zhao <zhaoyiming916@gmail.com>
2020-05-12 00:01:23 -0700

    registered service with Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { UsersController } from './controllers/users.controller';
+import { UsersService } from './services/users.service';
 import { AppService } from './app.service';
 
 @Module({ 
   imports: [],
   controllers: [AppController, UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
