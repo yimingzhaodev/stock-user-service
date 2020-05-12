a6ee56b2482dc7b08432d503568d077ca3a807b0
Yiming Zhao <zhaoyiming916@gmail.com>
2020-05-12 00:23:03 -0700

    test ssh

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -5,7 +5,6 @@ import { UsersService } from '../services/users.service';
 @Controller('users')
 export class UsersController {
   constructor(private usersService : UsersService) {}
-
   @Get()
   async getUsers(): Promise<User[]> {
     return this.usersService.getAllUsers();
