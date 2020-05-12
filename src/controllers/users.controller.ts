e81c5a4a1a47aae409ba6976c2a48ef96b50d333
Yiming Zhao <zhaoyiming916@gmail.com>
2020-05-12 00:10:56 -0700

    test ssh

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -4,7 +4,6 @@ import { UsersService } from '../services/users.service';
 
 @Controller('users')
 export class UsersController {
-  
   constructor(private usersService : UsersService) {}
 
   @Get()
