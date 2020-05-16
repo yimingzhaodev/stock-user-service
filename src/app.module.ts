0fb5e0b5ec845320015913861a56d7471a3c1dd2
Yiming Zhao <zhaoyiming916@gmail.com>
2020-05-15 23:04:08 -0700

    added postgres docker image script and other files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module'
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
 })
 export class AppModule {}
