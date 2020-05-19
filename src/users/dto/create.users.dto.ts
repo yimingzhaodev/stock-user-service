bb6dce20fa3878798f4acc3be6f2e0ba12aaf9c5
Yiming Zhao <zhaoyiming916@gmail.com>
2020-05-18 21:56:53 -0700

    created test

--- src/users/dto/create.users.dto.ts
+++ src/users/dto/create.users.dto.ts
@@ -2,5 +2,5 @@ import { IsNotEmpty } from 'class-validator';
 
 export class CreateUserDTO {
   @IsNotEmpty()
-  name: string;
+  public name: string;
 }
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import {UsersController} from "./users.controller";
 import {Test} from "@nestjs/testing";
 import {User} from "./model/user.entity";
 import {UsersRepository} from "./users.repository";
+import {CreateUserDTO} from "./dto/create.users.dto";
 
 describe('UsersController', () => {
     let usersService : UsersService;
@@ -11,7 +12,10 @@ describe('UsersController', () => {
     beforeEach(async () => {
         const moduleRef = await Test.createTestingModule({
             controllers: [UsersController],
-            providers: [UsersService, UsersRepository]
+            providers: [
+                UsersService,
+                UsersRepository,
+            ]
         }).compile();
 
         usersService = moduleRef.get<UsersService>(UsersService);
@@ -20,9 +24,27 @@ describe('UsersController', () => {
 
     describe('get',  () => {
         it('should return an array of users', async () => {
-            const testUsers = [];
-            jest.spyOn(usersService, "findAll").mockImplementation(() => testUsers);
-            expect(await usersController.get()).toBe(testUsers);
+            const spy = jest
+                .spyOn(usersController, 'get')
+                .mockImplementation(async () => [])
+            const users = await usersController.get();
+            expect(users).toStrictEqual([]);
+        });
+    })
+
+    describe('post',  () => {
+        it('should add an user', async () => {
+            const createUserDTO = new CreateUserDTO();
+            createUserDTO.name = 'test'
+
+            const spy = jest.spyOn(usersService, 'add');
+            await usersController.add(createUserDTO) 
+
+            const addedUser = usersService.findAll().find(user => user.name == 'test');
+
+            expect(spy).toBeCalledWith(createUserDTO);
+            expect(addedUser).toBeDefined();
+            expect(addedUser.name).toEqual(createUserDTO.name);
         });
     })
 });
